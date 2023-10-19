351541d5bfb12c27bf25fb4a8adeddd1c6b5d410
Hui:D <44632544+LeeSeongHui@users.noreply.github.com>
2023-10-19 14:23:15 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -3,7 +3,9 @@ cnt = N
 for _ in range(N):
     S = list(input()) 
     chk = [S[0]]
-    for i in range(len(S)-1): 
+    for i in range(len(S)-1):
+        # 앞뒤 알파벳을 비교해서 같으면 계속검사, 다르면 체크리스트에 추가
+        # 체크리스트 추가시 이미 있는 알파벳이면 cnt-1(그룹함수에서 제외)   
         if S[i] == S[i+1]:
             continue
         else:
@@ -11,5 +13,5 @@ for _ in range(N):
                 chk.append(S[i+1])
             else:
                 cnt-=1 
-                break   
+                break    
 print(cnt)
