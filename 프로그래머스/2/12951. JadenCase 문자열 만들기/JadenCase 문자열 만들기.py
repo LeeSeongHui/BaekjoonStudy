e842f3f9abf6bff199a788911923c9ac366c4fb3
Hui:D <44632544+LeeSeongHui@users.noreply.github.com>
2023-10-27 10:46:13 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -1,24 +1,19 @@
 # def solution(s): 
-
-#     seq = s.split()
-#     print(seq)
+#     seq = s.split(' ')
 #     answer = []
 #     for word in seq:
-#         if word[0].isalpha() == True : # 첫 문자가 알파벳일 떄
+#         if word : 
 #             answer.append(word[0].upper() + word[1:].lower())
 #         else :
-#             answer.append(word[0] + word[1:].lower())
-    
-#     return ' '.join(answer)  
-
-
+#             answer.append(word)
+#     return ' '.join(answer)     
+        
 def solution(s):
     answer = []
-    s = s.split(" ")
-    for word in s:
-        print(word)
+    seq = s.split(' ')
+    for word in seq:
         if word:
             answer.append(word[0].upper() + word[1:].lower())
         else:
             answer.append(word)
-    return " ".join(answer)
+    return ' '.join(answer)  
